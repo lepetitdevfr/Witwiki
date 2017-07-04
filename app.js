69bb31709ca28e4708af15c2e5a590dcc0c05f9e
cabri89 <cabridens@gmail.com>
2017-07-04 15:33:49 +0200

    change arbo

--- app.js
+++ app.js
@@ -11,25 +11,25 @@
         $routeProvider
             .when('/', {
                 controller: 'HomeController',
-                templateUrl: 'home/home.view.html',
+                templateUrl: 'view/home.view.html',
                 controllerAs: 'vm'
             })
 
             .when('/login', {
                 controller: 'LoginController',
-                templateUrl: 'login/login.view.html',
+                templateUrl: 'view/login.view.html',
                 controllerAs: 'vm'
             })
 
             .when('/register', {
                 controller: 'RegisterController',
-                templateUrl: 'register/register.view.html',
+                templateUrl: 'view/register.view.html',
                 controllerAs: 'vm'
             })
 
             // .when('/admin', {
             //     controller: 'AdminController',
-            //     templateUrl: 'admin/admin.view.html',
+            //     templateUrl: 'controller/admin.view.html',
             //     controllerAs: 'vm'
             // })
 
---
+++ controller/admin.controller.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+
+    angular
+    .module('app')
+    .controller('AdminController', HomeController);
+
+    AdminController.$inject = ['UserService', '$rootScope'];
+    function HomeController(UserService, $rootScope) {
+        var vm = this;
+
+        vm.updateUser = function(user) {
+            UserService.UpdateUSer(user);
+        }
+
+        vm.addUser = function(user) {
+            UserService.UpdateUSer(user);
+        }
+
+        function loadAllUsers() {
+            UserService.GetAll()
+            .then(function (users) {
+                vm.allUsers = users;
+            });
+        }
+    }
+
+})();
