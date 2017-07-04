96b330a2cbeb27d70a7a5858f931421eb87a6908
Lepetitdev <jerem71100@gmail.com>
2017-07-04 16:01:35 +0200

    add node server

---
+++ node-Wiki/server.js
@@ -0,0 +1,77 @@
+var express    = require('express');     
+var app        = express();               
+var bodyParser = require('body-parser');
+var mysql      = require('mysql');
+var passwordHash = require('password-hash');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'witwiki'
+});
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+app.get('/getUser',function (req,res) {
+
+	connection.query('SELECT * FROM user', function (error, results, fields) {
+		if (error) throw error;
+		console.log(results);
+		res.json(results);
+	});
+})
+
+app.post('/addUser', function(req, res, next) {
+	var params = req.body;
+	console.log(params);
+	var pswd = passwordHash.generate(params.password);
+
+
+	connection.query('INSERT INTO user (pseudo, lastname, firstname, email, password, id_role) VALUES ("'+params.pseudo+'","'+params.lastname+'","'+params.firstname+'","'+params.email+'","'+pswd+'","3")', function (error, results, fields) {
+		if (error) {
+			console.log(error.code); 
+			console.log(error.fatal);
+			res.json(error.code)
+		}else{
+			res.json(results);
+		}
+	});
+});
+
+app.post('/login', function (req, res, next) {
+	var params = req.body;
+	connection.query("SELECT * FROM user WHERE pseudo='"+params.pseudo+"'",function (error, results, fields) {
+		if (error) {
+			console.log(error.code); 
+			res.json(error.code)
+		}else{
+			console.log(results);
+			if (passwordHash.verify(params.password, results[0].password)) {
+				res.json({code:200,data:results[0]});
+			}else{
+				res.json({code:500,message:"Identifiant ou Mot de passe incorrecte."})
+			}
+			// res.json(results);
+
+
+		}
+	})
+})
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
