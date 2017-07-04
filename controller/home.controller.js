5d6c2d3fdda735ced1cb716942ad283d979ff72a
Lepetitdev <jerem71100@gmail.com>
2017-07-04 23:50:48 +0200

    css

--- controller/home.controller.js
+++ controller/home.controller.js
@@ -10,24 +10,15 @@
         var vm = this;
 
         vm.user = null;
-        vm.allUsers = [];
 
         initController();
 
         function initController() {
             loadCurrentUser();
-            loadAllUsers();
         }
 
         function loadCurrentUser() {
             vm.user = $rootScope.globals.currentUser;
-        }
-
-        function loadAllUsers() {
-            UserService.GetAllUsers()
-                .then(function (users) {
-                    vm.allUsers = users;
-                });
         }
     }
 
--- node-Wiki/server.js
+++ node-Wiki/server.js
@@ -5,10 +5,10 @@ var bodyParser = require('body-parser');
 var mysql      = require('mysql');
 var passwordHash = require('password-hash');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'witwiki'
+	host     : 'localhost',
+	user     : 'root',
+	password : 'root',
+	database : 'witwiki'
 });
 
 // configure app to use bodyParser()
@@ -23,24 +23,24 @@ var port = process.env.PORT || 8080;        // set our port
 var router = express.Router();              // get an instance of the express Router
 
 app.get('/getAllUsers',function (req,res) {
+	console.log("getAllUsers");
 	connection.query('SELECT pseudo, lastname, firstname, email, id_role FROM user', function (error, results, fields) {
 		if (error) throw error;
-		console.log(results);
 		res.json(results);
 	});
 })
 
 app.get('/getAllRoles',function (req,res) {
+	console.log("getAllRoles");
 	connection.query('SELECT * FROM role', function (error, results, fields) {
 		if (error) throw error;
-		console.log(results);
 		res.json(results);
 	});
 })
 
 app.post('/addUser', function(req, res, next) {
+	console.log("addUser");
 	var params = req.body;
-	console.log(params);
 	var pswd = passwordHash.generate(params.password);
 	connection.query('INSERT INTO user (pseudo, lastname, firstname, email, password, id_role) VALUES ("'+params.pseudo+'","'+params.lastname+'","'+params.firstname+'","'+params.email+'","'+pswd+'","3")', function (error, results, fields) {
 		if (error) {
@@ -52,28 +52,28 @@ app.post('/addUser', function(req, res, next) {
 });
 
 app.post('/login', function (req, res, next) {
+	console.log("login");
 	var params = req.body;
 	connection.query("SELECT * FROM user WHERE pseudo='"+params.pseudo+"'",function (error, results, fields) {
-		console.log(results);
 		try{
 			if (passwordHash.verify(params.password, results[0].password)) {
-					delete results[0].password;
-					res.json({code:200,data:results[0]});
-				}else{
-					res.json({code:500,message:"Identifiant ou Mot de passe incorrecte."})
-				}
+				delete results[0].password;
+				res.json({code:200,data:results[0]});
+			}else{
+				res.json({code:500,message:"Identifiant ou Mot de passe incorrecte."})
+			}
 		}catch(e){
-				console.log(e);
-				res.json({code:404,message:"Identifiant inconnu."})
+			console.log(e);
+			res.json({code:404,message:"Identifiant inconnu."})
 
 		}
 	})
 });
 
 app.post('/updateRole', function (req,res,next) {
+	console.log('updateRole');
 	var params = req.body;
 	connection.query("UPDATE user SET id_role="+params.role+" WHERE pseudo="+params.pseudo , function (error, results, fields) {
-		console.log(results);
 		try{
 			res.json({code:200});
 		}catch(e){
