735816dd076aea9e97627883d7911226d1cd1d76
Lepetitdev <jerem71100@gmail.com>
2017-07-05 15:42:34 +0200

    libs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('app', ['ngRoute', 'ngCookies', 'ngTable'])
+        .module('app', ['ngRoute', 'ngCookies', 'ngTable', 'textAngular'])
         .config(config)
         .run(run);
 
--- controller/addArticle.controller.js
+++ controller/addArticle.controller.js
@@ -5,12 +5,12 @@
         .module('app')
         .controller('addArticleController', addArticleController);
 
-    addArticleController.$inject = ['ArticleService', '$rootScope'];
-    function addArticleController(ArticleService, $rootScope) {
+    addArticleController.$inject = ['ArticleService', '$rootScope', 'textAngularManager'];
+    function addArticleController(ArticleService, $rootScope, textAngularManager) {
         var vm = this;
 
         initController();
-
+        console.log(textAngularManager);
         function initController() {
             
         }
