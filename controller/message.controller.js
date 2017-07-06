19ec56a6aa8cf8bdd7262b2c6a52a29b1b9ec2ab
cabri89 <cabridens@gmail.com>
2017-07-06 23:21:15 +0200

    *

--- controller/message.controller.js
+++ controller/message.controller.js
@@ -5,14 +5,29 @@
     .module('app')
     .controller('MessageController', MessageController);
 
-    MessageController.$inject = ['$rootScope','$location'];
-    function MessageController($rootScope, $location) {
+    MessageController.$inject = ['$rootScope','$location', 'UserService'];
+    function MessageController($rootScope, $location, UserService) {
         var vm = this;
 
         initController();
 
         function initController() {
+            loadDest();
+        }
 
+        vm.newMessage = function() {
+            var message = {title:vm.title,content:vm.message,to:vm.dest,from:$rootScope.id};
+            console.log(vm.dest);
+            console.log(message);
+
+        }
+
+        function loadDest() {
+            UserService.GetAllUsers()
+            .then(function (content) {
+                vm.dests =content.data;
+                console.log(vm.dests);
+            });
         }
     }
 
