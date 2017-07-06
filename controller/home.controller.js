8381b3822a6b09e0ed904db4b694c1681a6d6d8a
cabri89 <cabridens@gmail.com>
2017-07-06 09:55:39 +0200

    add pagination

--- controller/home.controller.js
+++ controller/home.controller.js
@@ -8,10 +8,31 @@
     HomeController.$inject = ['ArticleService', '$rootScope','CatService'];
     function HomeController(ArticleService, $rootScope, CatService) {
         var vm = this;
+        var numberArticle = 23;
+
+        vm.changeReq = function(pagination) {
+            console.log(vm.cat);
+            console.log(vm.tri);
+            
+            if (pagination != undefined) {
+                vm.pagination = pagination;
+            }
+            console.log(vm.pagination);
+        }
 
         function init() {
             loadAllArticles();
             loadAllCat();
+            pagination();
+
+            vm.tri = "date";
+            vm.cat = "all";
+
+            var paginationObject = {};
+            paginationObject.page = 1;
+            paginationObject.sqlInfo = 10;
+
+            vm.pagination = paginationObject;
         }
 
         function loadAllArticles() {
@@ -26,6 +47,19 @@
             .then(function (content) {
                 vm.allCat = content.data;
             });
+        }
+
+        function pagination() {
+            var nbrPage = Math.ceil(numberArticle / 10);
+            vm.paginationArray = [];
+
+            for (var i = 0; i < nbrPage; i++) {
+                var paginationObject = {};
+                var page = i+1;
+                paginationObject.page = page;
+                paginationObject.sqlInfo = page*10;
+                vm.paginationArray.push(paginationObject);
+            }
         }
 
         init();
