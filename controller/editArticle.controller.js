862f1d32b5fd67d33edba443b244b1e13f1eb40e
Lepetitdev <jerem71100@gmail.com>
2017-07-06 15:27:27 +0200

    edit article

--- controller/editArticle.controller.js
+++ controller/editArticle.controller.js
@@ -5,8 +5,8 @@
     .module('app')
     .controller('EditArticleController', EditArticleController);
 
-    EditArticleController.$inject = ['ArticleService', '$rootScope', 'textAngularManager', 'CatService', '$routeParams'];
-    function EditArticleController(ArticleService, $rootScope, textAngularManager, CatService, $routeParams) {
+    EditArticleController.$inject = ['ArticleService', '$rootScope', 'textAngularManager', 'CatService', '$routeParams','$location'];
+    function EditArticleController(ArticleService, $rootScope, textAngularManager, CatService, $routeParams,$location) {
         var vm = this;
 
         initController();
@@ -16,11 +16,14 @@
             loadArticle();
         }
 
-        vm.addArticle = function () {
-            var params = {title:vm.title,preface:vm.preface,content:vm.content,idCat:vm.idCat};
+        vm.editArticle = function () {
+            var params = {title:vm.title,preface:vm.preface,content:vm.content,idCat:vm.idCat,id:$routeParams.id};
             ArticleService.UpdateArticle(params)
             .then(function (response) {
                 console.log(response);
+                if (response.success) {
+                    $location.path('/article/'+$routeParams.id);
+                }
             });
         }
 
@@ -30,6 +33,10 @@
             .then(function (content) {
                 vm.article = content.data;
                 console.log(vm.article);
+                vm.content = vm.article.content;
+                vm.title = vm.article.title;
+                vm.preface = vm.article.preface;
+                vm.idCat = vm.article.catId;
             });
         }
 
